35b58d459738fe67b5430aa1fcb36f60e23e50b0
Guru-Sandhu <arsh3874@gmail.com>
2020-06-29 17:44:18 -0700

    added response caching

--- src/requests/index.js
+++ src/requests/index.js
@@ -2,7 +2,7 @@ import { API_KEY } from '../secrets'
 const BASE_URL = 'https://api.nasa.gov/planetary/apod'
 
 export function apod(date) {
-  return fetch(`${BASE_URL}?api_key=${API_KEY}&date=${date}`)
+  return fetch(`${BASE_URL}?api_key=${API_KEY}&date=${date}`, { cache: 'default'})
     .then(res => res.json())
     .then(data => data)
 }
